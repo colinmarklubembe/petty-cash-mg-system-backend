0a76b265a0c9fce9db7332e45c45c7f25e3811f5
colinmarklubembe <107845562+colinmarklubembe@users.noreply.github.com>
2024-07-22 14:56:13 +0300

    chore: Refactor authenticate middleware to use company instead of organization

--- src/api/middleware/authenticate.ts
+++ src/api/middleware/authenticate.ts
@@ -4,50 +4,52 @@ import { responses } from "../../utils";
 
 interface AuthenticatedRequest extends Request {
   user?: { email: string };
-  organization?: { organizationId: string };
+  company?: { companyId: string };
 }
 
-const authenticateToken = (
-  req: AuthenticatedRequest,
-  res: Response,
-  next: NextFunction
-) => {
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
+class Authenticate {
+  authenticateToken = (
+    req: AuthenticatedRequest,
+    res: Response,
+    next: NextFunction
+  ) => {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
 
-  if (!token)
-    return responses.errorResponse(res, 401, "Unauthorized! No token found");
+    if (!token)
+      return responses.errorResponse(res, 401, "Unauthorized! No token found");
 
-  jwt.verify(token, process.env.JWT_SECRET!, (err, user) => {
-    if (err)
+    jwt.verify(token, process.env.JWT_SECRET!, (err, user) => {
+      if (err)
+        return responses.errorResponse(
+          res,
+          403,
+          "Invalid token! Please login again"
+        );
+
+      req.user = user as { email: string; organizationId: string };
+      next();
+    });
+  };
+
+  checkCompanyId = (
+    req: AuthenticatedRequest,
+    res: Response,
+    next: NextFunction
+  ) => {
+    const companyId = req.headers["companyId"];
+
+    if (!companyId) {
       return responses.errorResponse(
         res,
-        403,
-        "Invalid token! Please login again"
+        400,
+        "comoanyId is required but was not found in the headers"
       );
+    }
 
-    req.user = user as { email: string; organizationId: string };
+    req.company = { companyId: companyId as string };
     next();
-  });
-};
+  };
+}
 
-const checkOrganizationId = (
-  req: AuthenticatedRequest,
-  res: Response,
-  next: NextFunction
-) => {
-  const organizationId = req.headers["organization-id"];
-
-  if (!organizationId) {
-    return responses.errorResponse(
-      res,
-      400,
-      "organization-id is required but was not found in the headers"
-    );
-  }
-
-  req.organization = { organizationId: organizationId as string };
-  next();
-};
-
-export default { authenticateToken, checkOrganizationId };
+export default new Authenticate();
